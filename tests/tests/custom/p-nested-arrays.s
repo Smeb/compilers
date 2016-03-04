d49784b7835dc1d92fe2767fa13f9437bda8a80a
Smeb <bryves@gmail.com>
2016-03-04 17:23:50 +0000

    super amazing working parser

--- tests/tests/custom/p-nested-arrays.s
+++ tests/tests/custom/p-nested-arrays.s
@@ -1,3 +1,5 @@
 main {
-	foo.children[6][4] = 2;
+	{1:1, 2:2, 3:"i hate parsers"}[6][4].dan.hekla[3][2][1].arewedone[:1] = 2;
+	foo()[6][4].dan.hekla[3][2][1].arewedone[:1] = 2;
+	"string"[6][4].dan.hekla[3][2][1].arewedone[:1] = 2;
 };
